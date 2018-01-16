c61c861acde53c9ee4c45284e4a5dd77b6e16120
Kait Mikitin <kait.mikitin@gmail.com>
2018-01-16 16:46:18 -0600

    finished creating routes and views for the house model

--- controllers/houseController.js
+++ controllers/houseController.js
@@ -3,7 +3,8 @@ const router = express.Router()
 
 const House = require('../models/housesModel.js')
 
-
+// INDEX ROUTE
+// POST ROUTE
 router.route('/') 
     .get( (req,res) => {
     House.find({}, (err, allHouses)=>{
@@ -12,8 +13,7 @@ router.route('/')
       })  
     })
     
-})
-.post((req,res) => {
+}).post((req,res) => {
     House.create(req.body, (err, createdHouse) => {
         if(err) console.log(err);
         console.log("hit post route for new")
@@ -23,20 +23,41 @@ router.route('/')
     })
 } )
 
+// NEW ROUTE
 router.get('/new', (req,res) => {
     res.render("./house/new.ejs")
 })
 
 
-
+// SHOW ROUTE
+// PUT ROUTE
+// DELETE ROUTE
 router.route('/:id')
-    .delete((req,res)=>{
-    House.findByIdAndRemove(req.params.id, (err, response) => {
-        if(err) {
-            console.log(err)
-        } else {
-            res.redirect("/houses")
-        }
+    .get((req, res) => {
+        House.findById(req.params.id, (err, foundHouse) => {
+            res.render('./house/show.ejs', { house: foundHouse })
+        })
+    }).put((req, res) => {
+        House.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            (err, updatedHouse) => {
+                res.redirect('/houses')
+            })
+    }).delete((req,res)=>{
+         House.findByIdAndRemove(req.params.id, (err, response) => {
+            if(err) {
+                console.log(err)
+             } else {
+                res.redirect("/houses")
+            }
+    })
+})
+
+// EDIT ROUTE
+router.get('/:id/edit', (req, res) => {
+    House.findById(req.params.id, (err, foundHouse) => {
+        res.render('./house/edit.ejs', { house: foundHouse })
     })
 })
 
